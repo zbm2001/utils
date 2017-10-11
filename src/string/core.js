6b959b59213a4feb5a28a33f1384b42ba7ba3c86
zhoubaimin <zhoubaimin@zhuaqianmao.com>
2017-10-11 19:13:40 +0800

    modify string includes() repeat() startsWith() endsWith() padStart() padEnd()

--- src/string/core.js
+++ src/string/core.js
@@ -25,25 +25,80 @@ if (!isNativeFunction(StringProto.repeat)) {
       }
       if (n === 1) return s
       s += s
-      n = n >> 1 //相当于将n除以2取其商，或者说是开2次方
+      n = n >> 1 // 相当于将 n 除以 2 取其商，或者说是开 2 次方
     }
     return s
   }
 
-  StringProto.includes = function includes (str) {
+  StringProto.includes = function includes (search, start) {
+    if (typeof start !== 'number') start = 0
+
+    if (start + search.length > this.length) {
+      return false
+    } else {
+      return this.indexOf(search, start) > -1
+    }
   }
 
-  StringProto.startsWith = function startsWith (str) {
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+   * @param searchString
+   * @param position
+   * @returns {boolean}
+   */
+  StringProto.startsWith = function startsWith (searchString, position){
+    return this.substr(position || 0, searchString.length) === searchString
   }
 
-  StringProto.endsWith = function endsWith (str) {
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+   * @param searchStr
+   * @param position
+   * @returns {boolean}
+   */
+  StringProto.endsWith = function endsWith (searchStr, position) {
+    // This works much better than >= because
+    // it compensates for NaN:
+    if (!(position < this.length)) position = this.length
+    else position |= 0 // round position
+    return this.substr(position - searchStr.length, searchStr.length) === searchStr
   }
 }
 
 if (!isNativeFunction(StringProto.padEnd)) {
-  StringProto.padStart = function padStart (length, str) {
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+   * @param targetLength
+   * @param padString
+   * @returns {string}
+   */
+  StringProto.padStart = function padStart (targetLength, padString) {
+    targetLength = targetLength >> 0 //floor if number or convert non-number to 0;
+    padString = String(padString || ' ')
+    if (this.length > targetLength) return String(this)
+
+    targetLength = targetLength - this.length
+    if (targetLength > padString.length) {
+      padString += padString.repeat(targetLength / padString.length) //append to original to ensure we are longer than needed
+    }
+    return padString.slice(0, targetLength) + String(this)
   }
 
-  StringProto.padEnd = function padEnd (length, str) {
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padEnd
+   * @param targetLength
+   * @param padString
+   * @returns {string}
+   */
+  StringProto.padEnd = function padEnd (targetLength, padString) {
+    targetLength = targetLength >> 0 //floor if number or convert non-number to 0;
+    padString = String(padString || ' ')
+    if (this.length > targetLength) return String(this)
+
+    targetLength = targetLength - this.length
+    if (targetLength > padString.length) {
+      padString += padString.repeat(targetLength / padString.length) //append to original to ensure we are longer than needed
+    }
+    return String(this) + padString.slice(0, targetLength)
   }
 }
